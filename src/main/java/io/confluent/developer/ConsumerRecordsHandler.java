bb1bd2b4aa1d1f20cc9c7653bc4a36a4db4e92b9
Sandesh Karambettu <skarambettu@confluent.io>
2023-04-20 09:43:57 -0700

    first commit

---
+++ src/main/java/io/confluent/developer/ConsumerRecordsHandler.java
@@ -0,0 +1,8 @@
+package io.confluent.developer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+public interface ConsumerRecordsHandler<K, V> {
+   void process(ConsumerRecords<K, V> consumerRecords);
+}
+
